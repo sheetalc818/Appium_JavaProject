77cb89814cab3725f6ea42be6ce2b123445a2103
sheetalc818 <sheetalc818@gmail.com>
2022-05-26 16:13:12 +0530

    Removed : Unused imports

--- src/main/java/com/bridgelabz/appium/base/Flipkart1.java
+++ src/main/java/com/bridgelabz/appium/base/Flipkart1.java
@@ -1,22 +1,12 @@
 package com.bridgelabz.appium.base;
 
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.PerformsTouchActions;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Flipkart1 {
